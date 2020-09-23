bb0aad293b7738fc9e45e4d7d9f8032269466e4b
xzhula96092600 <164914434@qq.com>
2020-09-23 21:24:28 +0800

    Initial commit

---
+++ user-parent/user-dao/src/main/java/com/qfedu/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.qfedu.dao;
+
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> findAll();
+
+}
---
+++ user-parent/user-dao/src/main/java/com/qfedu/dao/impl/UserDaoImpl.java
@@ -0,0 +1,14 @@
+package com.qfedu.dao.impl;
+
+import com.qfedu.dao.User;
+import com.qfedu.dao.UserDao;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public List<User> findAll() {
+        return Arrays.asList(new User("xz", 23, "男"), new User("xzz", 23, "男"));
+    }
+}
